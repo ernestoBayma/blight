285c745c161d93621309e50d6baf2b05a1891a1f
Ernesto Bayma <ernestobayma@gmail.com>
2023-11-20 16:45:03 -0300

    New_feature: Adding textures
    Uses the stb_image lib to load textures to OpenGL.

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,34 @@
 
 #include <Shader.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 #define GLFW_RESIZE_CALLBACK(name) void name(GLFWwindow* window, int width, int height)
 GLFW_RESIZE_CALLBACK(framebuffer_resize_callback) {
 	glViewport(0, 0, width, height);
 }
 
 static float vertices[] = {
-	  // positions ( left, right, top )  // colors
-	 -0.5f, -0.5f, 0.0f, 		     1.0f, 0.0f , 0.0f, // bottom right
-	  0.5,  -0.5f, 0.0f, 		     0.0f, 1.0f , 0.0f, // bottom left
-	  0.0,   0.5f, 0.0f,		     0.0f, 0.0f , 1.0f  // top
+	  // positions   		     // colors		  // texture coords
+	  0.5f, 0.5f, 0.0f, 		     1.0f, 0.0f, 0.0f,    1.0f, 1.0f,  // top right
+	  0.5f, -0.5f, 0.0f, 		     0.0f, 1.0f, 0.0f,    1.0f, 0.0f,  // bottom right
+	 -0.5f, -0.5f, 0.0f,		     0.0f, 0.0f, 1.0f,    0.0f, 0.0f,  // bottom left
+	 -0.5f,  0.5f, 0.0f,		     1.0f, 1.0f, 0.0f,    0.0f, 1.0f   // top left
 };
-
+static int indices[] = {
+	0, 1, 3, // first triangle
+	1, 2, 3  // second triangle
+};
 
 void process_input(GLFWwindow *window);
 
 int main(int argc, char **argv) 
 {
 GLFWwindow 	*window;
-unsigned int 	VBO, VAO;
-char	  	*vertex_shader_src, *frag_shader_src;
-int		success, vertex_color_location;
-float		timeValue, greenValue;
+unsigned int 	VBO, VAO, EBO, texture;
+int		success, width, height, channels;
+unsigned char   *data = NULL;
 
 	if(!glfwInit()) return -1;
 
@@ -55,21 +61,50 @@ float		timeValue, greenValue;
 		return -1;
 	}
 
-	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, 0);	
-	glBindVertexArray(0);
+
+	// Texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	data = stbi_load("res/container.jpg", &width, &height, &channels, 0);
+	if(data == NULL) {
+		fprintf(stderr, "Failure to load image\n");
+		return -1;
+	}
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
+
+	shader_prog.use();
+	shader_prog.setUniformInt("texture1", 0);
 
 	while(!glfwWindowShouldClose(window)) {
 		process_input(window);
@@ -77,10 +112,12 @@ float		timeValue, greenValue;
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		shader_prog.use();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
 
+		shader_prog.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -88,6 +125,7 @@ float		timeValue, greenValue;
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
